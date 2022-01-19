9fb41b60a30d17788ffbe992892bf319983810ba
akchindo <abdullahichindo16@gmail.com>
2022-01-19 15:48:12 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById('root'))
+
+ReactDOM.render(<ul><li>I am Abdullai Chindo</li>
+                    <li>aspiring frontend</li>
+</ul>, document.getElementById('root'))
